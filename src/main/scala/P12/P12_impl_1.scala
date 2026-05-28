9988bdde390461cc1993370c4acae16ae48d3077
FFucks <fabio_geiss@hotmail.com>
2026-05-27 21:45:35 -0300

    Added P12 description

--- src/main/scala/P12/P12_impl_1.scala
+++ src/main/scala/P12/P12_impl_1.scala
@@ -1,5 +1,14 @@
 package P12
 
+/*
+P12 (**) Decode a run-length encoded list.
+Given a run-length code list generated as specified in problem P10, construct its uncompressed version.
+
+Example:
+
+scala> decode(List((4, 'a), (1, 'b), (2, 'c), (2, 'a), (1, 'd), (4, 'e)))
+res0: List[Symbol] = List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)
+ */
 import scala.collection.mutable.ListBuffer
 
 class P12_impl_1 {
