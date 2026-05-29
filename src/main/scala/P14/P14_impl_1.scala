d8f4e73a6e3708ad7a262eb93dc0ce0da48536fd
FFucks <fabio_geiss@hotmail.com>
2026-05-28 21:02:09 -0300

    Implement P14

---
+++ src/main/scala/P14/P14_impl_1.scala
@@ -0,0 +1,37 @@
+package P14
+
+/*
+P14 (*) Duplicate the elements of a list.
+Example:
+
+scala> duplicate(List('a, 'b, 'c, 'c, 'd))
+res0: List[Symbol] = List('a, 'a, 'b, 'b, 'c, 'c, 'c, 'c, 'd, 'd)
+ */
+
+import scala.collection.mutable.ListBuffer
+
+class P14_impl_1 {
+
+    def duplicate[A](list: List[A]): List[A] = {
+
+        val result = ListBuffer[A]()
+
+        for (element <- list) {
+            result += element
+            result += element
+        }
+
+        result.toList
+    }
+}
+
+@main def P14Impl1(): Unit = {
+
+    val dpk013 = new P14_impl_1
+
+    val input = List("a", "b", "c", "c", "d")
+
+    val result = dpk013.duplicate(input)
+
+    println(result)
+}
