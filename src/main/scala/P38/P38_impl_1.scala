c1cda26d6f04703b280f54253c64648bbcc73076
FFucks <fabio_geiss@hotmail.com>
2026-07-15 20:11:21 -0300

    Implement P38

--- src/main/scala/P38/P38_impl_1.scala
+++ src/main/scala/P38/P38_impl_1.scala
@@ -3,6 +3,12 @@ package P38
 import P34.P34_impl_1
 import P37.P37_impl_1
 
+/* P38 (*) Compare the two methods of calculating Euler’s totient function.
+Use the solutions of problems P34 and P37 to compare the algorithm. Try to calculate
+ϕ(10090)
+ϕ(10090) as an example.
+*/
+
 class P38_impl_1 {
 
     private val p34 = new P34_impl_1
