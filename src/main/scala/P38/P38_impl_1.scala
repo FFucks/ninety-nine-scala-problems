0384a8dc1aa11d5dc29c45f6b15877f8f03b1e78
FFucks <fabio_geiss@hotmail.com>
2026-07-15 20:10:21 -0300

    Implement P38

---
+++ src/main/scala/P38/P38_impl_1.scala
@@ -0,0 +1,41 @@
+package P38
+
+import P34.P34_impl_1
+import P37.P37_impl_1
+
+class P38_impl_1 {
+
+    private val p34 = new P34_impl_1
+    private val p37 = new P37_impl_1
+
+    def compare(number: Int): Unit = {
+
+        var startTime = System.nanoTime()
+        val resultP34 = p34.totient(number)
+        var endTime = System.nanoTime()
+
+        val p34Time = endTime - startTime
+
+        startTime = System.nanoTime()
+        val resultP37 = p37.totient(number)
+        endTime = System.nanoTime()
+
+        val p37Time = endTime - startTime
+
+        println(s"P34 Result : $resultP34")
+        println(s"P34 Time   : $p34Time ns")
+
+        println()
+
+        println(s"P37 Result : $resultP37")
+        println(s"P37 Time   : $p37Time ns")
+    }
+}
+
+@main
+def P38Impl1(): Unit = {
+
+    val dpk038 = new P38_impl_1
+
+    dpk038.compare(10090)
+}
