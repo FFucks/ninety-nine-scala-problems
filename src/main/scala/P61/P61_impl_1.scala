f92a84e3cdf35e6600e4047682c481f19f2d2b22
FFucks <fabio_geiss@hotmail.com>
2026-08-06 20:29:33 -0300

    Implement P61

--- src/main/scala/P61/P61_impl_1.scala
+++ src/main/scala/P61/P61_impl_1.scala
@@ -5,6 +5,12 @@ P61 (*) Count the leaves of a binary tree.
 A leaf is a node with no successors.  Write a method leafCount to count them.
 scala> Node('x', Node('x'), End).leafCount
 res0: Int = 1
+
+P61A (*) Collect the leaves of a binary tree in a list.
+A leaf is a node with no successors.  Write a method leafList to collect them in a list.
+
+scala> Node('a', Node('b'), Node('c', Node('d'), Node('e'))).leafList
+res0: List[Char] = List(b, d, e)
  */
 
 import Tree.*
@@ -26,14 +32,42 @@ class P61_impl_1 {
     }
 }
 
+
+class P61A_impl_1 {
+
+    def leafList[T](tree: Tree[T]): List[T] = {
+
+        if (tree == End) {
+            return List.empty[T]
+        }
+        val node = tree.asInstanceOf[Node[T]]
+
+        if (node.left == End && node.right == End) {
+            return List(node.value)
+        }
+
+        leafList(node.left) ++ leafList(node.right)
+    }
+}
+
+
 @main
 def P61Impl1(): Unit = {
 
     val dpk061 = new P61_impl_1
 
-    val tree = Node('x', Node('x'), End)
+    val tree = Node('a', Node('b'), Node('c', Node('d'), Node('e')))
 
-    val result = dpk061.leafCount(tree)
+    println(s"Leaf quantity: ${dpk061.leafCount(tree)}")
+}
 
-    println(s"Leaf Quantity: $result")
+
+@main
+def P61AImpl1(): Unit = {
+
+    val dpk061A = new P61A_impl_1
+
+    val tree = Node('a', Node('b'), Node('c', Node('d'), Node('e')))
+
+    println(s"Leaf: ${dpk061A.leafList(tree)}")
 }
